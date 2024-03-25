e8de72fa1f1f04f727af151329a9013a4c747b05
zwd <17326195691@163.com>
2024-03-25 10:58:49 +0800

    chore: add lint-staged and prettier

--- scripts/verify-commit.ts
+++ scripts/verify-commit.ts
@@ -12,15 +12,15 @@ if (!commitRE.test(msg)) {
   console.log(pico.yellow(`\n你提交的信息: ${msg}\n`));
   console.error(
     `  ${pico.white(pico.bgRed(" 错误 "))} ${pico.red(
-      `无效的提交信息格式.`
+      `无效的提交信息格式.`,
     )}\n\n` +
       pico.red(`  正确的提交消息格式. 例如:\n\n`) +
       `    ${pico.green(`feat: add a new feature`)}\n` +
       `    ${pico.green(`fix: fixed an interaction bug`)}\n\n` +
       pico.red(
         `我们参考了Vue3的方案.\n` +
-          `访问 https://github.com/vuejs/core/blob/main/.github/commit-convention.md 查看更多细节.\n`
-      )
+          `访问 https://github.com/vuejs/core/blob/main/.github/commit-convention.md 查看更多细节.\n`,
+      ),
   );
   process.exit(1);
 }
